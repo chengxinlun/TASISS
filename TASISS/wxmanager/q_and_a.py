dec63027aeae673545f3a46347e38bb7206cd12f
chengxinlun <chengxinlun@gmail.com>
2016-11-26 16:35:37 +0800

    Added time checking functionality

--- TASISS/wxmanager/q_and_a.py
+++ TASISS/wxmanager/q_and_a.py
@@ -4,22 +4,28 @@ import wechat.official as off
 
 
 def stamp2date(timestamp):
-    return datetime.datetime.fromtimestamp(int(timestamp)).replace(tzinfo=pytz.UTC)
+    return datetime.datetime.fromtimestamp(
+        int(timestamp)).replace(tzinfo=pytz.UTC)
 
 
 def q_and_a(req):
     timestamp = req.CreateTime
     inputtime = stamp2date(timestamp)
+    operation = [datetime.datetime(2016, 11, 27, 11, 30, 0, 0, pytz.UTC),
+                 datetime.datetime(2016, 11, 27, 14, 0, 0, 0, pytz.UTC)]
+    if inputtime < operation[0] or inputtime > operation[1]:
+        return off.WxEmptyResponse(req)
     text = "欢迎来到天文夜！您需要完成的答题环节在以下连接中，" + \
-        "请根据参与的项目完成相应的题目。完成后将获得相应奖励。" + \
+        "请根据参与的项目完成相应的题目。请将至少三份得分为满分的问卷的完成" + \
+        "界面截图并在奖品兑换处出示给工作人员，即可获得相应奖品。\n" + \
         '<a href="https://ks.sojump.hk/jq/10881815.aspx">' + \
-        "ETX125</a>，" + \
+        "ETX125望远镜</a>，" + \
         '<a href="https://ks.sojump.hk/jq/10881870.aspx">' + \
-        'Meadal130</a>，' + \
+        'Meade 130</a>，' + \
         '<a href="https://ks.sojump.hk/jq/10880154.aspx">' + \
-        "EQ3D</a>，" + \
+        "信达小黑牛反</a>，" + \
         '<a href="https://ks.sojump.hk/jq/10880047.aspx">' + \
-        "GSO RC8</a>，" + \
+        "GSO RC8望远镜</a>，" + \
         '<a href="https://ks.sojump.hk/jq/10880491.aspx">' + \
         "其他题目</a>"
     return off.WxTextResponse(text, req)
